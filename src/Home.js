87c6e92bb5e731da0117379d92b02bf8bda26e08
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-02-21 22:50:55 +0900

    create page added

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,14 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'Hello World', body: 'My first blog has...', author: 'momoko', id: 1 },
-    { title: 'I am tired', body: 'Todday was such a busy...', author: 'momoko', id: 2 },
-    { title: 'Studying English', body: 'English is a difficult...', author: 'momoko', id: 3 },
-  ])
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs!" />
+      { error && <div>{ error }</div>}
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title="All blogs!" />}
     </div>
   );
 }
