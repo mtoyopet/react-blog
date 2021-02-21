5ee6d6d6916f1393a69b1dc932e1ae6c27acea9f
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-02-21 23:36:13 +0900

    delete button added

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,22 @@
 import { useParams } from 'react-router-dom'
 import useFetch from './useFetch'
+import { useHistory } from 'react-router-dom' 
 
 const BlogDetails = () => {
   const { id } = useParams()
   const { data: blog, isPending, error } = useFetch(`http://localhost:8000/blogs/${id}`)
+  const history = useHistory()
 
+  const handleDelete = (e) => {
+    e.preventDefault()
+
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: 'DELETE',
+      headers: { "Content-Type": "application/json" }
+    }).then((res) => {
+      history.push('/')
+    })
+  }
   return ( 
     <div className="blog-details">
       { isPending && <div>Loading...</div> }
@@ -16,6 +28,7 @@ const BlogDetails = () => {
           <div>{ blog.body }</div>
         </article>
       )}
+      { !isPending && <button onClick={handleDelete}>Delete</button> }
     </div>
   );
 }
